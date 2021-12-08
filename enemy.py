a1ea5a715585a4faa0827282692e6857ecd30550
nando42930 <nandjinhoterra@hotmail.com>
2021-12-08 03:02:59 +0000

    enemy.py updated

--- enemy.py
+++ enemy.py
@@ -1,6 +1,22 @@
-class Enemy:
+from abc import ABCMeta
+
+class Enemy(metaclass=ABCMeta):
+
+    attack_impact = None
 
     def __init__(self, strength, attacks, health):
+        self.strenght = None
+        self.attacks = None
+        self.health = None
+
+    def has_attacked(self):
+        self.attacks -= 1
+
+    def was_attacked(self, health):
+        self.health -= health
+
+    @abstractmethod
+    def calc_impact(self):
 
 
 class Tank(Enemy):
@@ -10,6 +26,9 @@ class Tank(Enemy):
         super().attacks = 2
         super().health = 200
 
+    def calc_impact(self):
+        self.attack_impact = strength * health / 200
+
 
 class Artillery(Enemy):
 
@@ -18,6 +37,9 @@ class Artillery(Enemy):
         super().attacks = 1
         super().health = 50
 
+    def calc_impact(self):
+        self.attack_impact = strength * health / 50
+
 
 class Infantry(Enemy):
 
@@ -25,3 +47,6 @@ class Infantry(Enemy):
         super().strength = 100
         super().attacks = 3
         super().health = 100
+
+    def calc_impact(self):
+        self.attack_impact = strength * health / 100
